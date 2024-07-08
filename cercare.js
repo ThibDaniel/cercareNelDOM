11eb2a3a761b583ab485b23ce3bfd013577336f1
ThibDaniel <160511547+ThibDaniel@users.noreply.github.com>
2024-07-08 16:57:09 +0200

    Create cercare.js

---
+++ cercare.js
@@ -0,0 +1,8 @@
+const elementiClasseP = document.querySelectorAll('.paragrafo');
+console.log("Elementi con classe 'paragrafo':", elementiClasseP);
+
+const elementoConId3 = document.getElementById('elemento-3');
+console.log("Elemento con id 'elemento-3':", elementoConId3);
+
+const h2Element = document.getElementsByTagName('h2');
+console.log("Tutti gli elementi h2 della pagina:", h2Element);
